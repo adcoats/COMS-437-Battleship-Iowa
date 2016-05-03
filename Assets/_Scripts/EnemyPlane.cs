73561431b546305c25bb9b7b894d7274acff8458
Alec Lofquist <alces14@gmail.com>
2016-05-03 15:06:02 -0500

    Starting on EnemyPlane AI

--- Assets/_Scripts/EnemyPlane.cs
+++ Assets/_Scripts/EnemyPlane.cs
@@ -5,14 +5,38 @@ public class EnemyPlane : MonoBehaviour {
 
 	public GameObject planeExplosion;
 
+    // AI
+    public GameObject target;
+
+    public float speed = 10.0f;
+
+    private const float CIRCLE_RADIUS = 10.0f;
+    private const float APPROACH_DISTANCE = 2.0f;
+
 	// Use this for initialization
-	void Start () {
+	void Start()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+    {
+        Vector2 dir = target.transform.position - transform.position;
+        float dist = dir.magnitude;
+        dir.Normalize();
+
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+
+        if (dist > APPROACH_DISTANCE)
+        {
+            // approach
+            float force = body.velocity.magnitude - speed;
+            body.AddForce(dir * speed);
+        } else {
+            // circle
+
+        }
 	}
 
     void OnDestroyed()
