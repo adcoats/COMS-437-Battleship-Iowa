12c0f7e39f355b8bdcb989f788a3cdaa33360ba0
Alec Lofquist <alces14@gmail.com>
2016-05-04 17:08:03 -0500

    Moved VectorAngleWithSign to Util.cs

---
+++ Assets/_Scripts/Util.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util
+{
+    // http://answers.unity3d.com/questions/162177/vector2angles-direction.html
+    // pure fucking magic
+    public static float VectorAngleWithSign(Vector2 a, Vector2 b)
+    {
+        float ang = Vector2.Angle(a, b);
+        Vector3 cross = Vector3.Cross(a, b);
+
+        if (cross.z > 0)
+            ang = 360 - ang;
+
+        if (ang > 180)
+            ang = -(360 - ang);
+
+        return -ang;
+    }
+}
